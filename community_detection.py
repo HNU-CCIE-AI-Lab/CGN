851efae0ea11150bb51ab2040bd54164685c6c56
HNU-CCIE-AI-Lab <106733939+HNU-CCIE-AI-Lab@users.noreply.github.com>
2022-08-12 23:57:50 +0800

    Update community_detection.py

--- community_detection.py
+++ community_detection.py
@@ -16,10 +16,9 @@ import scipy.sparse as sp
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import average_precision_score
 
-from gae.optimizer import OptimizerAE, OptimizerVAE
-from gae.input_data_club import load_data
-from gae.model import GCNModelAE, GCNModelVAE
-from gae.preprocessing import preprocess_graph, construct_feed_dict, sparse_to_tuple, mask_test_edges
+
+from input_data_club import load_data
+
 from igraph import *
 import igraph
 from sklearn import metrics
@@ -29,44 +28,4 @@ def community_detection(g,algorithm):
         community = g.community_multilevel()
         return community
 
-    if algorithm == "eigenvector":
-        community = g.community_leading_eigenvector(clusters=50, arpack_options=None)
-        # community = g.community_leading_eigenvector()
-        return community
-    if algorithm == "fastgreedy":
-        community = g.community_fastgreedy()
-        community = community.as_clustering()
-        return community
-    if algorithm == "infomap":
-        community = g.community_infomap()
-        return community
-    if algorithm == "label_propagation":
-        community = g.community_label_propagation()
-        return community
-    if algorithm == "edge_betweenness":
-        # community = g.community_edge_betweenness(clusters=200,directed=False)
-        community = g.community_edge_betweenness()
-        community = community.as_clustering()
-        return community
-    if algorithm == "spinglass":
-        community = g.community_spinglass(weights=None, spins=100, parupdate=False, start_temp=1, stop_temp=0.01, cool_fact=0.99, update_rule="config", gamma=1, implementation="orig", lambda_=1)
-        # community = g.community_spinglass()
-        return community
-    if algorithm == "walktrap":
-        community = g.community_walktrap()
-        community = community.as_clustering()
-        return community
-
-# adj, features = load_data('dblp') # pubmed,cora,citeseer,karate,miserables,words,nets,power,dblp
-# # print(adj)
-#
-# G = nx.from_scipy_sparse_matrix(adj) #nx的图
-# adj = nx.to_numpy_array(G)
-# adj = adj - np.diag(np.diag(adj))
-#
-#
-# adj = nx.to_numpy_array(G) # igraph的图
-# g = Graph.Adjacency(adj.astype(bool).tolist(),mode='undirected')
-#
-# community = community_detection(g,'infomap')
-# print(community)
+    
